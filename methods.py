ac6e2d6d08b661c47a64a52db7e600583ac24cfb
rm2816 <rm2816@ic.ac.uk>
2021-07-22 16:45:07 +0100

    Added bertweet optimisation code

--- methods.py
+++ methods.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import re
 
 
 def read_model_data(path):
     data = pd.read_csv(path)
     data = data.dropna()
     data["Relevance"] = data["Relevance"].apply(lambda x: int(x))
+    data["Text"] = data["Text"].apply(lambda x: re.sub("&amp;", "and", x))
     return data
