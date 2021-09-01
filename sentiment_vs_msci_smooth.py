e356f02013e833a99cc6c70eae9fa95e2f4686a9
rm2816 <rm2816@ic.ac.uk>
2021-09-01 18:28:25 +0100

    Renamed files

--- sentiment_vs_msci_smooth.py
+++ sentiment_vs_msci_smooth.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-from correlation_raw import name_translation, get_indices, get_sentiment, print_corr
+from sentiment_vs_msci_raw import name_translation, get_indices, get_sentiment, print_corr
 
 def loc_eval(x, b):
     loc_est = 0
@@ -52,79 +52,64 @@ def loess(yvals, data, alpha, poly_degree=1):
 
 
 def smooth_data(sent_data, index_data, name):
+    min_date = pd.Timestamp(2010, 1, 1)
+    max_date = pd.Timestamp(2020, 12, 1)
     monthly = sent_data[sent_data["Text"].str.contains(name_translation.get(name), case=False)]
     monthly = monthly.groupby(pd.Grouper(key="Date", freq="1M")).sum().filter(regex="Total")
     ind = index_data[index_data.clean_name == name].groupby(pd.Grouper(key="AS_OF_DATE", freq="1M")).sum(min_count=1)
+    ind = ind[min_date:]
     ind = ind.interpolate(method="pad")
+    ind = ind.dropna()
 
-    min_date = pd.Timestamp(2010, 1, 1)
-    max_date = pd.Timestamp(2020, 12, 1)
+    # Smooth
 
-    # Plot sentiment
     eval_sent = loess("Total", data=monthly, alpha=0.15, poly_degree=2)
     eval_sent["v"] = eval_sent["v"].apply(lambda x: datetime.fromtimestamp(x))
-    fig = plt.figure(figsize=(15, 10))
-    ax1 = fig.add_subplot(111)
-    ax1.plot(eval_sent['v'], eval_sent['g'], color='red', linewidth=3, label="Sentiment")
-    plt.title('Sustainability Sentiment for ' + name_translation.get(
-        name) + ' with LOWESS, alpha = 0.15, polynomial degree = 2')
-    plt.xlabel("Date")
-    plt.ylabel("Sentiment")
-    plt.xlim((min_date, max_date))
-    plt.legend()
-    plt.tight_layout()
-    plt.savefig("data/lowess/" + name_translation.get(name) + "_sentiment.png", dpi=150)
-    plt.close(fig)
-
-    # Plot indices
-    evalDF_ind = loess("INDUSTRY_ADJUSTED_SCORE", data=ind, alpha=0.18, poly_degree=2)
     evalDF_env = loess("ENVIRONMENTAL_PILLAR_SCORE", data=ind, alpha=0.18, poly_degree=2)
     evalDF_soc = loess("SOCIAL_PILLAR_SCORE", data=ind, alpha=0.18, poly_degree=2)
-    evalDF_ind["v"] = evalDF_ind["v"].apply(lambda x: datetime.fromtimestamp(x))
     evalDF_env["v"] = evalDF_env["v"].apply(lambda x: datetime.fromtimestamp(x))
     evalDF_soc["v"] = evalDF_soc["v"].apply(lambda x: datetime.fromtimestamp(x))
-    fig2 = plt.figure(figsize=(15, 10))
-    ax2 = fig2.add_subplot(111)
-    ax2.plot(evalDF_ind['v'], evalDF_ind['g'], color='blue', linewidth=3, label="Industry")
-    ax2.plot(evalDF_env['v'], evalDF_env['g'], color='green', linewidth=3, label="Environmental")
-    ax2.plot(evalDF_soc['v'], evalDF_soc['g'], color='orange', linewidth=3, label="Social")
-    plt.title('Sustainbility Indices for ' + name_translation.get(
-        name) + ' with LOWESS, alpha = 0.18, polynomial degree = 2')
+
+    # Plot combined
+
+    fig3 = plt.figure(figsize=(9, 3.5))
+    ax3 = fig3.add_subplot(111)
+    ax4 = ax3.twinx()
+    ax3.plot(eval_sent['v'], eval_sent['g'], color='red')
+    ax4.plot(evalDF_env['v'], evalDF_env['g'], color='green')
+    ax4.plot(evalDF_soc['v'], evalDF_soc['g'], color='orange')
     plt.xlabel("Date")
-    plt.ylabel("Score")
+    ax3.set_ylabel("Tweet Sentiment Count")
+    ax4.set_ylabel("Index Score")
+    ax3.set_xlabel("Date")
     plt.xlim((min_date, max_date))
-    plt.legend()
     plt.tight_layout()
-    ax2.get_figure().savefig("data/lowess/" + name_translation.get(name) + "_indices.png",
-                             dpi=150)
-    plt.close(fig2)
+    plt.savefig("data/lowess/report/other_smoothed_" + name_translation.get(name) + ".png", dpi=400)
+    plt.close(fig3)
 
     # Find correlation (no lag)
 
     evalDF = eval_sent.groupby(pd.Grouper(key="v", freq="1M")).sum().rename(columns={"g": "sentiment"})
-    evalDF_ind = evalDF_ind.groupby(pd.Grouper(key="v", freq="1M")).sum().rename(columns={"g": "industry"})
     evalDF_env = evalDF_env.groupby(pd.Grouper(key="v", freq="1M")).sum().rename(columns={"g": "environment"})
     evalDF_soc = evalDF_soc.groupby(pd.Grouper(key="v", freq="1M")).sum().rename(columns={"g": "social"})
-    eval_tot = pd.concat([evalDF, evalDF_ind, evalDF_env, evalDF_soc], axis=1)
+    eval_tot = pd.concat([evalDF, evalDF_env, evalDF_soc], axis=1)
     no_lag_corr = eval_tot.corr()
     no_lag_corr.to_csv("data/lowess/no_lag_" + name_translation.get(name) + ".csv")
-    print("Industry correlation:", no_lag_corr.sentiment.iloc[1])
-    print("Environmental correlation:", no_lag_corr.sentiment.iloc[2])
-    print("Social correlation:", no_lag_corr.sentiment.iloc[3])
+    print("Environmental correlation:", no_lag_corr.sentiment.iloc[1])
+    print("Social correlation:", no_lag_corr.sentiment.iloc[2])
 
     # Find correlation (with lag)
 
     corr_list = []
-    for i in range(-24, 25):
+    for i in range(-12, 13):
         eval_temp = eval_tot.copy(deep=True)
         eval_temp.sentiment = eval_temp.sentiment.shift(i)
         corr = eval_temp.corr()
-        corr_list.append(pd.DataFrame([[i, corr.sentiment.iloc[1], corr.sentiment.iloc[2], corr.sentiment.iloc[3]]],
-                                      columns=["Shift", "Industry", "Environmental", "Social"]))
+        corr_list.append(pd.DataFrame([[i, corr.sentiment.iloc[1], corr.sentiment.iloc[2]]],
+                                      columns=["Shift", "Environmental", "Social"]))
 
     results = pd.concat(corr_list, ignore_index=True)
     results.to_csv("data/lowess/lag_" + name_translation.get(name) + ".csv")
-    print_corr(results, "Industry")
     print_corr(results, "Environmental")
     print_corr(results, "Social")
 
--- svm_optimisation.py
+++ svm_optimisation.py
@@ -4,7 +4,7 @@ import methods
 import numpy as np
 from svm_model import SvmModel, SvmTokenizer
 from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn import svm
 import seaborn as sns
@@ -12,21 +12,23 @@ import seaborn as sns
 word_vector_size = 300
 
 if __name__ == '__main__':
+    np.random.seed(42)
 
     tokenizer = SvmTokenizer()
-    tokenizer.train_embedding("data/all/clean.csv", "data/svm/word_model_svm.kv", word_vector_size)
+    tokenizer.train_embedding("data/all/clean_filtered.csv", "data/svm/word_embeddings.kv", word_vector_size)
 
     model_data = methods.read_model_data("data/all/mum_filtered_with_duplicates.csv")
     model_data = SvmModel.preprocess(model_data)
 
-    word_train = tokenizer.apply(model_data)
+    train_text, temp_text, train_labels, temp_labels = train_test_split(model_data['Text'], model_data['Relevance'],
+                                                                        shuffle=True, test_size=0.2, random_state=42)
+    val_text, test_text, val_labels, test_labels = train_test_split(temp_text, temp_labels, shuffle=True,
+                                                                    test_size=0.5, random_state=42)
 
-    train_count = int(word_train.shape[0] * 0.8)
-
-    train_text = word_train[:train_count, :word_vector_size]
-    test_text = word_train[train_count:, :word_vector_size]
-    train_labels = word_train[:train_count, word_vector_size:].ravel()
-    test_labels = word_train[train_count:, word_vector_size:].ravel()
+    train_text = train_text.append(val_text)
+    train_labels = train_labels.append(val_labels)
+    train_text = tokenizer.apply(train_text)
+    test_text = tokenizer.apply(test_text)
 
     scaler = StandardScaler().fit(train_text)
     train_text = scaler.transform(train_text)
@@ -37,31 +39,40 @@ if __name__ == '__main__':
     grid.fit(train_text, train_labels)
     print(grid.best_estimator_)
     grid_predictions = grid.predict(test_text)
-    joblib.dump(grid.best_estimator_, "data/svm/best_model_3000.pkl")
+    joblib.dump(grid.best_estimator_, "data/svm/best_model_3000_correct.pkl")
     print(classification_report(test_labels, grid_predictions))
     cr = classification_report(test_labels, grid_predictions, output_dict=True)
     df = pd.DataFrame(cr).transpose()
-    df.to_html("data/svm/report_3000.html")
+    df.to_html("data/svm/report_3000_correct.html")
     cf_matrix = confusion_matrix(test_labels, grid_predictions)
     cm_plot = sns.heatmap(cf_matrix / np.sum(cf_matrix), annot=True, fmt='.2%', cmap='Blues')
     cm_plot.get_figure().savefig("data/svm/cm_svm_3000.png")
 
+    cf_matrix = confusion_matrix(test_labels, grid_predictions)
+    print(cf_matrix)
+
+    cf_matrix = confusion_matrix(test_labels, grid_predictions, normalize='true')
+    sns.set(font_scale=1.2)
+    cf_plot = sns.heatmap(cf_matrix, annot=True,
+                          fmt='.2%', cmap='Blues')
+    cf_plot.get_figure().savefig("data/svm/cf_plot_correct.png", dpi=400)
+
     # Best model result:
-    # C=0.1, tol=1e-05
+    # C=0.001, tol=1e-05
     #
-    #                 precision    recall  f1-score   support
+    #               precision    recall  f1-score   support
     #
-    #          0.0       0.45      0.25      0.32       117
-    #          1.0       0.85      0.93      0.89       527
+    #            0       0.68      0.55      0.61        98
+    #            1       0.82      0.89      0.85       224
     #
-    #     accuracy                           0.81       644
-    #    macro avg       0.65      0.59      0.60       644
-    # weighted avg       0.78      0.81      0.79       644
+    #     accuracy                           0.79       322
+    #    macro avg       0.75      0.72      0.73       322
+    # weighted avg       0.78      0.79      0.78       322
     #
     # Confusion matrix:
-    # [[ 29  88]
-    #  [ 35 492]]
+    # [[ 54  44]
+    #  [ 25 199]]
     #
     # Normalised:
-    # [[0.25 0.75]
-    #  [0.07 0.93]]
+    # [[0.55 0.45]
+    #  [0.11 0.89]]
